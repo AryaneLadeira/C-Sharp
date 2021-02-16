f816f8c9153404984c8fbd492a85fe7cbc76d599
AryaneLadeira <aryanemottals@gmail.com>
2021-02-16 17:36:42 -0300

    Exercicio 2

---
+++ Exercicios Classes/Exercicio 2/ConsoleApp1/ConsoleApp1/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Pessoa
+    {
+        public double Peso
+        {
+            get { return peso; }
+
+            set { peso = value; }
+
+        }
+        public double Altura
+        {
+            get { return altura; }
+            set { altura = value; } 
+        }
+
+        private double peso;
+        private double altura;
+    }
+}
---
+++ Exercicios Classes/Exercicio 2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+
+            Console.WriteLine("Qual é o seu peso?");
+            pessoa1.Peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Qual é a sua altura?");
+            pessoa1.Altura = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nDados recebidos:");
+            Console.WriteLine("Peso: {0}", pessoa1.Peso);
+            Console.WriteLine("Altura: {0}", pessoa1.Altura);
+        }
+    }
+}
