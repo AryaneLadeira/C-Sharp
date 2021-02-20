33030b6fbc173a42922ef7cbf8bdf25233ac57cf
AryaneLadeira <aryanemottals@gmail.com>
2021-02-20 15:00:41 -0300

    Exercicio 4

---
+++ Exercicios Classes/Exercicio 4/ConsoleApp1/ConsoleApp1/Contato.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Contato
+    {
+        public string Nome 
+        {
+            get { return nome; }
+            set { nome = value; } 
+        
+        }
+
+        public string Logradouro
+        {
+            get { return logradouro; }
+            set { logradouro = value; }
+
+        }
+
+        public int Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+
+        }
+
+        public string Complemento
+        {
+            get { return complemento; }
+            set { complemento = value; }
+
+        }
+        public string Bairro
+        {
+            get { return bairro; }
+            set { bairro = value; }
+
+        }
+
+        public string Cidade
+        {
+            get { return cidade; }
+            set { cidade = value; }
+
+        }
+
+        public string UF
+        {
+            get { return uf; }
+            set { uf = value; }
+
+        }
+
+        public string Telefone
+        {
+            get { return telefone; }
+            set { telefone = value; }
+
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+
+        }
+
+        private string nome;
+        private string logradouro;
+        private int numero;
+        private string complemento;
+        private string bairro;
+        private string cidade;
+        private string uf;
+        private string telefone;
+        private string email;
+    }
+}
---
+++ Exercicios Classes/Exercicio 4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contato contato = new Contato();
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Digite os dados do novo contato");
+            Console.WriteLine("\nNome: ");
+            contato.Nome = Console.ReadLine();
+
+            Console.WriteLine("Endereço");
+            Console.WriteLine("   Logradouro: ");
+            contato.Logradouro = Console.ReadLine();
+
+            Console.WriteLine("   Número: ");
+            contato.Numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("   Complemento: ");
+            contato.Complemento = Console.ReadLine();
+
+            Console.WriteLine("   Bairro: ");
+            contato.Bairro = Console.ReadLine();
+
+            Console.WriteLine("   Cidade: ");
+            contato.Cidade = Console.ReadLine();
+
+            Console.WriteLine("   UF: ");
+            contato.UF = Console.ReadLine();
+
+            Console.WriteLine("Telefone: ");
+            contato.Telefone = Console.ReadLine();
+
+            Console.WriteLine("E-mail: ");
+            contato.Email = Console.ReadLine();
+
+            Console.WriteLine("---------------------------");
+
+            Console.WriteLine("\n{0}", contato.Nome);
+            Console.WriteLine("Rua {0}, {1}, {2}, {3}, {4} ", contato.Logradouro, contato.Complemento, contato.Bairro, contato.Cidade, contato.UF);
+
+            Console.WriteLine("Telefone: {0}", contato.Telefone);
+            Console.WriteLine("E-mail: {0}", contato.Email);
+        }
+    }
+}
