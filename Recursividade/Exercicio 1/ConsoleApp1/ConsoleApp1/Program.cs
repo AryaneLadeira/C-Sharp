7edba41f554c4d81226b0749f435e6ed0f4ed6b7
AryaneLadeira <aryanemottals@gmail.com>
2021-03-01 20:01:57 -0300

    Aula recursividade exercicio 1

---
+++ Recursividade/Exercicio 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Faça um programa que receba um inteiro por parâmetro e imprima todos os números pares entre 0 e o número passado por parâmetro.
+ Considere que serão passados somente inteiros positivos por parâmetro. */
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Imprimir(7);
+        }
+
+        static void Imprimir(int valor)
+        {
+            if(valor > 0)
+            Imprimir(valor - 1);
+
+            if (valor % 2 == 0)
+                Console.WriteLine(valor);
+        }
+
+    }
+}
