58356ca97fd40188ade1d2f9bc3db6e6b8127cbe
AryaneLadeira <aryanemottals@gmail.com>
2021-03-01 18:04:17 -0300

    Revisão Vetores 1

---
+++ Revisao Vetores/Exercicio 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] vetor = new double[5];
+
+            vetor[0] = 1;
+            vetor[1] = 0;
+            vetor[2] = 7;
+            vetor[3] = -58;
+            vetor[4] = -5;
+
+            NegativoParaPositivo(vetor);
+            LerVetor(vetor);
+            
+        }
+
+        static void NegativoParaPositivo(double[] vetor)
+        {
+            for(int i = 0; i < vetor.Length; i++)
+            {
+                if (vetor[i] < 0)
+                    vetor[i] *= -1;
+            }
+        }
+
+        static void LerVetor(double[] vetor)
+        {
+            for(int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine($"Valor {i}: {vetor[i]}");
+            }
+        }
+    }
+}
