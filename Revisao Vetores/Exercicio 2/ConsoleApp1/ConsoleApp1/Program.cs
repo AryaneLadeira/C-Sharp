42a1d8c3266b76fbb61250d66bf7c47cf64de502
AryaneLadeira <aryanemottals@gmail.com>
2021-03-01 18:13:41 -0300

    Revisao Vetores 2

---
+++ Revisao Vetores/Exercicio 2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[5];
+
+            vetor[0] = 45;
+            vetor[1] = 2;
+            vetor[2] = 67;
+            vetor[3] = 100;
+            vetor[4] = 1;
+
+            Console.WriteLine("O maior valor do vetor é: {0}", MaiorValor(vetor));
+        }
+
+        static int MaiorValor( int[] vetor)
+        {
+            int maiorvalor = vetor[0];
+
+            for(int i = 0; i < vetor.Length; i++)
+            {
+                if(vetor[i] > maiorvalor)
+                {
+                    maiorvalor = vetor[i];
+                }
+            }
+
+            return maiorvalor;
+        }
+    }
+}
